e7acef120393c1a518a0a5a61dfeaaa234722a48
Lrop <Lagropner04@gmail.com>
2019-08-10 14:55:05 -0600

    save

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,29 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+var friends = [
+	{
+		name: "Friend One",
+		photo: "",
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+	},
+	{
+		name: "Friend Two",
+		photo: "",
+		scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+	},
+	{
+		name: "Friend Three",
+		photo: "",
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},
+	{
+		name: "Friend Four",
+		photo: "",
+		scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	},
+	{
+		name: "Friend Five",
+		photo: "",
+		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	}
+];
 
-var app = express();
-
-var PORT = process.env.PORT || 8080;
+module.exports = friends;
