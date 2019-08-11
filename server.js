bf31afec9a0677d5de71ea78af392ac57e79964c
Lrop <Lagropner04@gmail.com>
2019-08-11 13:50:45 -0600

    save

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     // database: "freindFinder.DB"
+//   });
+  
+//   connection.connect(function(err) {
+//     if (err) {
+//       console.error("error connecting: " + err.stack);
+//       return;
+//     }
+  
+//     console.log("connected as id " + connection.threadId);
+//   });
+  
+
+
+
  app.use(bodyParser.json());
  app.use(bodyParser.urlencoded({extended: true})); 
  app.use(bodyParser.text());
@@ -18,4 +38,7 @@ app.use(express.static("app/public"));
 // require("./routing/htmlRoutes.js")(app);
 
 
-app.listen(PORT, () => console.log("Working", PORT))
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
