a9bf751d6cbd527619773428daa6de12d146122c
Lrop <Lagropner04@gmail.com>
2019-08-11 14:21:09 -0600

    save

--- server.js
+++ server.js
@@ -6,38 +6,10 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     // database: "freindFinder.DB"
-//   });
-  
-//   connection.connect(function(err) {
-//     if (err) {
-//       console.error("error connecting: " + err.stack);
-//       return;
-//     }
-  
-//     console.log("connected as id " + connection.threadId);
-//   });
-app.get('/', function (req, res) {
-    res.render('home', {});
-  });
 
 
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-// render the error page
-res.status(err.status || 500);
-res.send('error');//this or res.status(err.status || 500).send('error')
-});
+
  
-
-  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -48,4 +20,27 @@ app.use(express.static("app/public"));
 require("./routing/htmlRoutes")(app);
 require("./routing/apiRoutes")(app);
 
-app.listen(PORT, () => console.log("Listening on port %s", PORT));
+
+
+
+app.get('/', function (req, res) {
+    res.render('home', {});
+  });
+
+
+  app.use(function(err, req, res, next) {
+      
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+// render the error page
+res.status(err.status || 500);
+res.send('error');//this or res.status(err.status || 500).send('error')
+});
+   
+
+app.listen(PORT, function() {
+
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
