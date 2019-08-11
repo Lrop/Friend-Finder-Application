d971fd1b3d075a128c41cd48af20f65f85b86df7
Lrop <Lagropner04@gmail.com>
2019-08-11 13:46:47 -0600

    save

--- server.js
+++ server.js
@@ -5,3 +5,17 @@ var path = require("path");
 var app = express();
 
 var PORT = process.env.PORT || 8080;
+
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended: true})); 
+ app.use(bodyParser.text());
+ app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+
+app.use(express.static("app/public"));
+
+// require("./routing/apiRoutes.js")(app);
+// require("./routing/htmlRoutes.js")(app);
+
+
+app.listen(PORT, () => console.log("Working", PORT))
