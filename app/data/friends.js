e83083baa0804a78a4b7c5aec6d7537e67725973
Lrop <Lagropner04@gmail.com>
2019-08-13 21:22:38 -0600

    save

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,22 +7,26 @@ var friends = [
 	},
 	{
 		name: "Friend Two",
-		photo: "",
+		photo: "https://images.unsplash.com/photo-1531259683007-016a7b628fc3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80",
+		// batman
 		scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 	},
 	{
 		name: "Friend Three",
-		photo: "",
+		photo: "https://images.unsplash.com/photo-1534809027769-b00d750a6bac?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+		// spiderman
 		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 	},
 	{
 		name: "Friend Four",
-		photo: "",
+		photo: "https://images.unsplash.com/photo-1561149877-84d268ba65b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1875&q=80",
+		// captain america
 		scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
 	},
 	{
 		name: "Friend Five",
-		photo: "",
+		photo: "https://images.unsplash.com/photo-1532202802379-df93d543bac3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80",
+		// superdog
 		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 	}
 ];
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,10 +1,10 @@
 var path = require("path");
 
 module.exports = function(app) {
-    app.get("/public/home.html", function(req, res){
+    app.get("/", function(req, res){
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
-    app.get("/public/survey.html", function(req, res){
+    app.get("./survey", function(req, res){
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
 }
